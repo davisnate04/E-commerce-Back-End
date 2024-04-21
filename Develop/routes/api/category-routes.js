46a84a03e1921a3150e0b51da54d3c15cc2e308d
davisnate04 <davisnate04@gmail.com>
2024-04-20 23:05:19 -0400

    Adding completed files

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -3,26 +3,68 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
   // find all categories
+  const categories = await Category.findAll({
+    include: [{
+      model: Product,
+    }],
+  });
   // be sure to include its associated Products
+  res.status(200).json(categories);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+router.get('/:id', async (req, res) => {
+  try {
+      // find one category by its `id` value
+      const category = await Category.findOne({where: {id: req.params.id}, include: [{model: Product}]})
+      // be sure to include its associated Products
+      res.status(200).json(category);
+
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
   // create a new category
+  const category = await Category.create({ category_name: req.body.category_name });
+  
+  res.status(200).json(category);
+} catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+router.put('/:id', async (req, res) => {
+  try {// update a category by its `id` value
+  const category = await Category.update({ category_name: req.body.category_name }, {where: {id: req.params.id }});
+  
+  res.status(200).json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
   // delete a category by its `id` value
+  const category = await Category.destroy({where: {id: req.params.id}});
+
+  res.status(200).json(category);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,25 +3,64 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
+  try {
+    const tags = await Tag.findAll({include: {model: Product}});
   // find all tags
   // be sure to include its associated Product data
+    res.status(200).json(tags);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.findOne({where: {id: req.params.id}});
+
+    res.status(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
   // find a single tag by its `id`
   // be sure to include its associated Product data
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  try {
+    const tag = await Tag.create({tag_name: req.body.tag_name});
+
+    res.status(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.update({tag_name: req.body.tag_name}, {where: {id: req.params.id}});
+
+    res.status(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
   // update a tag's name by its `id` value
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+  try {
+    const tag = await Tag.destroy({where: {id: req.params.id}});
+    
+    res.status(200).json(tag);
+  } catch (err) {
+    console.log(err);
+    res.status(500).json(err);
+  }
   // delete on tag by its `id` value
 });
 
